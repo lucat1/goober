e17ca47fd1556abd4bb722c50573d8ba26583a1a
luca <lucat1@users.noreply.github.com>
2020-05-01 11:44:29 +0200

    :poop: quick fix

--- src/styled.js
+++ src/styled.js
@@ -1,15 +1,13 @@
 import { css } from './css';
 import { parse } from './core/parse';
 
-let h, forwardRef, useTheme;
-const setup = (pragma, prefix, fwd, theme) => {
+let h;
+const setup = (pragma, prefix) => {
     // This one needs to stay in here, so we won't have cyclic dependencies
     parse.p = prefix;
 
     // These are scope to this context
     h = pragma;
-    forwardRef = fwd;
-    useTheme = theme;
 };
 
 /**
@@ -22,7 +20,7 @@ function styled(tag) {
     return function wrapper() {
         const _args = arguments;
 
-        function Styled(props, ref) {
+        return function Styled(props, ref) {
             // Grab a shallow copy of the props
             // _ctx.p: is the props sent to the context
             const _props = (_ctx.p = Object.assign({ theme: useTheme && useTheme() }, props));
@@ -36,13 +34,8 @@ function styled(tag) {
             _props.className =
                 css.apply(_ctx, _args) + (_previousClassName ? ' ' + _previousClassName : '');
 
-            // Add the passed ref function to props
-            _props.ref = ref;
-
             return h(tag, _props);
-        }
-
-        return forwardRef ? forwardRef(Styled) : Styled;
+        };
     };
 }
 
